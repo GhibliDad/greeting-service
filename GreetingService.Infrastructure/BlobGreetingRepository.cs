b3afee396dbda91c25bf614e4da6d7e6f21eb661
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-02-15 15:23:40 +0100

    refactor

--- GreetingService.Infrastructure/BlobGreetingRepository.cs
+++ GreetingService.Infrastructure/BlobGreetingRepository.cs
@@ -23,19 +23,19 @@ namespace GreetingService.Infrastructure
             _blobContainerClient = new BlobContainerClient(connectionString, _blobContainerName);
             _blobContainerClient.CreateIfNotExists();
         }
-        public async Task<IEnumerable<Greeting>> GetAsync()
+
+        public async Task CreateAsync(Greeting greeting)
         {
-            var greetings = new List<Greeting>();
-            var blobs = _blobContainerClient.GetBlobsAsync();
-            await foreach (var blob in blobs)
-            {
-                var blobClient = _blobContainerClient.GetBlobClient(blob.Name);
-                var blobContent = await blobClient.DownloadContentAsync();
-                var greeting = blobContent.Value.Content.ToObjectFromJson<Greeting>();
-                greetings.Add(greeting);
-            }
-            
-            return greetings;
+            var content = File.ReadAllText(_filePath);
+            var greetings = JsonSerializer.Deserialize<IList<Greeting>>(content);
+            var existingGreeting = greetings?.FirstOrDefault(x => x.Id == greeting.Id);
+
+            if (greetings.Any(x => x.Id == greeting.Id))
+                throw new Exception($"Greeting with ID: {greeting.Id} already exists");
+
+            greetings.Add(greeting);
+
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(greetings, _jsonSerializerOptions));
         }
 
         public async Task<Greeting> GetAsync(Guid id)
@@ -51,19 +51,21 @@ namespace GreetingService.Infrastructure
             return null;
         }
 
-        public async Task CreateAsync(Greeting greeting)
+        public async Task<IEnumerable<Greeting>> GetAsync()
         {
-            var content = File.ReadAllText(_filePath);
-            var greetings = JsonSerializer.Deserialize<IList<Greeting>>(content);
-            var existingGreeting = greetings?.FirstOrDefault(x => x.Id == greeting.Id);
-
-            if (greetings.Any(x => x.Id == greeting.Id))
-                throw new Exception($"Greeting with ID: {greeting.Id} already exists");
-
-            greetings.Add(greeting);
-
-            File.WriteAllText(_filePath, JsonSerializer.Serialize(greetings, _jsonSerializerOptions));
+            var greetings = new List<Greeting>();
+            var blobs = _blobContainerClient.GetBlobsAsync();
+            await foreach (var blob in blobs)
+            {
+                var blobClient = _blobContainerClient.GetBlobClient(blob.Name);
+                var blobContent = await blobClient.DownloadContentAsync();
+                var greeting = blobContent.Value.Content.ToObjectFromJson<Greeting>();
+                greetings.Add(greeting);
+            }
+            
+            return greetings;
         }
+
         public async Task UpdateAsync(Greeting greeting)
         {
             throw new NotImplementedException();
