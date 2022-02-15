f837e9aa6369e122529c46036ecd142350580e54
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-02-15 14:19:47 +0100

    Blob Greeting Repository
    implemented interface!

---
+++ GreetingService.Infrastructure/BlobGreetingRepository.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Storage.Blobs;
+using GreetingService.Core.Entities;
+using GreetingService.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GreetingService.Infrastructure
+{
+    public class BlobGreetingRepository : IGreetingRepository
+    {
+        private const string _blobContainerName = "greetings";
+        private readonly BlobContainerClient _blobContainerClient;
+        private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true, };
+
+        public BlobGreetingRepository(IConfiguration configuration)
+        {
+            var connectionString = configuration["LogStorageAccount"];
+            _blobContainerClient = new BlobContainerClient(connectionString, _blobContainerName);
+            _blobContainerClient.CreateIfNotExists();
+        }
+        public Task<IEnumerable<Greeting>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Greeting> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(Greeting greeting)
+        {
+            throw new NotImplementedException();
+        }
+        public Task UpdateAsync(Greeting greeting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
