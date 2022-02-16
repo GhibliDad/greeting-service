0717d2ff69d2c0baab9e3bc13eb7e7724fc0e0b2
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-02-16 10:24:51 +0100

    Create BlobUserService.cs

---
+++ GreetingService.Infrastructure/BlobUserService.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Storage.Blobs;
+using GreetingService.Core;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GreetingService.Infrastructure
+{
+    public class BlobUserService : IUserService
+    {
+        private const string _blobContainerName = "greetings";
+        private readonly BlobContainerClient _blobContainerClient;
+        private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true, };
+
+        public BlobUserService(IConfiguration configuration)
+        {
+            var connectionString = configuration["LogStorageAccount"];
+            _blobContainerClient = new BlobContainerClient(connectionString, _blobContainerName);
+            _blobContainerClient.CreateIfNotExists();
+        }
+
+        public bool IsValidUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
