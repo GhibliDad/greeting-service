29d9e004dc2e7514a19ea1f667faaca50a977740
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-02-21 14:03:22 +0100

    Update SqlGreetingRepository.cs

--- GreetingService.Infrastructure/GreetingRepository/SqlGreetingRepository.cs
+++ GreetingService.Infrastructure/GreetingRepository/SqlGreetingRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GreetingService.Core.Entities;
+using GreetingService.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,36 @@ using System.Threading.Tasks;
 
 namespace GreetingService.Infrastructure.GreetingRepository
 {
-    internal class SqlGreetingRepository
+    internal class SqlGreetingRepository : IGreetingRepository
     {
+        public Task CreateAsync(Greeting greeting)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task DeleteAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Greeting> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Greeting>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Greeting greeting)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
