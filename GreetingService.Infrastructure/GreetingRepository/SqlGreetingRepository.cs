b97b82ebeaf8c7847a1956df2f30d9cfa690af5b
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-02-23 10:57:01 +0100

    Added Delete and DeleteAll methods

--- GreetingService.Infrastructure/GreetingRepository/SqlGreetingRepository.cs
+++ GreetingService.Infrastructure/GreetingRepository/SqlGreetingRepository.cs
@@ -32,7 +32,32 @@ namespace GreetingService.Infrastructure.GreetingRepository
 
         public async Task<IEnumerable<Greeting>> GetAsync()
         {
-            throw new NotImplementedException();
+            return await _greetingDbContext.Greetings.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Greeting>> GetAsync(string from, string to)
+        {
+            //from & to are not null
+            if (!string.IsNullOrWhiteSpace(from) && !string.IsNullOrWhiteSpace(to))
+            {
+                var greetings = _greetingDbContext.Greetings.Where(x => x.From.Equals(from) && x.To.Equals(to));
+                return await greetings.ToListAsync();
+            }
+            //from is not null & to is null
+            else if (!string.IsNullOrWhiteSpace(from) && string.IsNullOrWhiteSpace(to))
+            {
+                var greetings = _greetingDbContext.Greetings.Where(x => x.From.Equals(from));
+                return await greetings.ToListAsync();
+            }
+            //from is null & to is not null
+            else if (string.IsNullOrWhiteSpace(from) && !string.IsNullOrWhiteSpace(to))
+            {
+                var greetings = _greetingDbContext.Greetings.Where(x => x.To.Equals(to));
+                return await greetings.ToListAsync();
+            }
+
+            //from & to are null, return all greetings
+            return await _greetingDbContext.Greetings.ToListAsync();
         }
 
         public async Task UpdateAsync(Greeting greeting)
@@ -50,12 +75,26 @@ namespace GreetingService.Infrastructure.GreetingRepository
 
         public async Task DeleteAllAsync()
         {
-            throw new NotImplementedException();
+            var greetings = await _greetingDbContext.Greetings.ToListAsync();
+            if (!greetings.Any())
+                throw new Exception("No Greetings to delete");
+
+            greetings.Clear();
+
+            await _greetingDbContext.SaveChangesAsync();
         }
 
         public async Task DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var greetings = await _greetingDbContext.Greetings.ToListAsync();
+            var greeting = await _greetingDbContext.Greetings.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (greeting == null)
+                throw new Exception("Greeting does not exist");
+
+            greetings.Remove(greeting);
+
+            await _greetingDbContext.SaveChangesAsync();
         }
 
     }
