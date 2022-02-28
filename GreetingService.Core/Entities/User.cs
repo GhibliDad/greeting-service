5a6db308f8a3d0fe218fe71cdca703c3a0b8cc46
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-02-28 17:18:54 +0100

    Progress save

--- GreetingService.Core/Entities/User.cs
+++ GreetingService.Core/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GreetingService.Core.Exceptions;
+using GreetingService.Core.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,21 @@ namespace GreetingService.Core.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
+        private string _email;
+        public string Email 
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (!InputValidationHelper.IsValidEmail(value))
+                    throw new InvalidEmailException($"{value} is not a valid email.");
+
+                _email = value;
+            }
+        }
         public string Password { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime Modified { get; set; } = DateTime.Now;
---
+++ GreetingService.Core/Helpers/InputValidationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.Core.Helpers
+{
+    public class InputValidationHelper
+    {
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+
+            try
+            {
+
+            }
+            catch 
+            {
+
+            }
+        }
+    }
+}
