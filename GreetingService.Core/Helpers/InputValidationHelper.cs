99d823a264ad5913ae4f6c79f21bf19674240575
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-01 11:31:41 +0100

    Update InputValidationHelper.cs

--- GreetingService.Core/Helpers/InputValidationHelper.cs
+++ GreetingService.Core/Helpers/InputValidationHelper.cs
@@ -1,6 +1,7 @@
 ﻿using GreetingService.Core.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -12,18 +13,45 @@ namespace GreetingService.Core.Helpers
     {
         public static bool IsValidEmail(string email)
         {
-            if (string.IsNullOrEmpty(email))
+            if (string.IsNullOrWhiteSpace(email))
                 return false;
 
             try
             {
-                return Regex.IsMatch(email,
-                   @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
-                   RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
             }
-            catch 
+            catch (RegexMatchTimeoutException e)
             {
-                throw new Exception();
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
             }
         }
     }
