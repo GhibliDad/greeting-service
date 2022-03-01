b7fa3203d00d632747d2a0d14985cac1fe0b3825
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-01 13:38:06 +0100

    Created new class Invoice

---
+++ GreetingService.Core/Entities/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.Core.Entities
+{
+    internal class Invoice
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public List<Greeting> Greetings { get; set;}
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int CostPerGreeting { get; set; } = 20;
+        public int TotalCost { get; set; }
+        public string Currency { get; set; } = "SEK";
+    }
+}
