b04c2716655021c2517020e9ee94e6273700976d
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-01 13:41:17 +0100

    Create IInvoiceService.cs

---
+++ GreetingService.Core/Interfaces/IInvoiceService.cs
@@ -0,0 +1,16 @@
+﻿using GreetingService.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.Core.Interfaces
+{
+    internal interface IInvoiceService
+    {
+        public Task<IEnumerable<Invoice>> GetInvoices(int year, int month);
+        public Task<Invoice> GetInvoice(int year, int month, string email);
+        public Task CreateOrUpdateInvoice(Invoice invoice);
+    }
+}
