c8ba2950d41128f0276c4324f4aff946198ca6f5
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-07 11:13:39 +0100

    First commit of branch

---
+++ GreetingService.Core/Interfaces/IMessagingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.Core.Interfaces
+{
+    public interface IMessagingService
+    {
+        public Task SendAsync<T>(T message, string subject);
+    }
+}
---
+++ GreetingService.Infrastructure/UserService/ServiceBusMessagingService.cs
@@ -0,0 +1,17 @@
+﻿using GreetingService.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.Infrastructure.UserService
+{
+    internal class ServiceBusMessagingService : IMessagingService
+    {
+        public Task SendAsync<T>(T message, string subject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
