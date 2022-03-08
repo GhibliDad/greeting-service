e99d7a8d43a506726def205b6d3263390fac4bd2
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-08 14:50:08 +0100

    Update SbCreateUser.cs

--- GreetingService.API.Function/Users/SbCreateUser.cs
+++ GreetingService.API.Function/Users/SbCreateUser.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading.Tasks;
+using GreetingService.Core.Interfaces;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,14 +10,17 @@ namespace GreetingService.API.Function.Users
     public class SbCreateUser
     {
         private readonly ILogger<SbCreateUser> _logger;
+        private readonly IGreetingRepository _greetingRepository;
 
-        public SbCreateUser(ILogger<SbCreateUser> log)
+        public SbCreateUser(ILogger<SbCreateUser> log, IGreetingRepository greetingRepository)
         {
             _logger = log;
+            _greetingRepository = greetingRepository;
+
         }
 
         [FunctionName("SbCreateUser")]
-        public void Run([ServiceBusTrigger("main", "user_create", Connection = "ServiceBusConnectionString")]string mySbMsg)
+        public async Task Run([ServiceBusTrigger("main", "user_create", Connection = "ServiceBusConnectionString")]string mySbMsg)
         {
             _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
         }
