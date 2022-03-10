960770422d3d94d8c871c67e063e495cda4b1938
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-10 11:35:02 +0100

    commitment

---
+++ GreetingService.Infrastructure/ApprovalService/IApprovalService.cs
@@ -0,0 +1,6 @@
+﻿namespace GreetingService.Infrastructure.ApprovalService
+{
+    internal interface IApprovalService
+    {
+    }
+}
---
+++ GreetingService.Infrastructure/ApprovalService/TeamsApprovalService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.Infrastructure.ApprovalService
+{
+    internal class TeamsApprovalService : IApprovalService
+    {
+        public TeamsApprovalService()
+        {
+        }
+    }
+}
