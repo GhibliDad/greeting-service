f43be5160557a374b3c6d5403edfe6905844ed9c
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-14 11:26:04 +0100

    added approve/reject endpoints

---
+++ GreetingService.API.Function/Users/ApproveUser.cs
@@ -0,0 +1,43 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using GreetingService.API.Function.Authentication;
+using GreetingService.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace GreetingService.API.Function.Users
+{
+    public class ApproveUser
+    {
+        private readonly ILogger<ApproveUser> _logger;
+        private readonly IUserService _userService;
+        private readonly IAuthHandler _authHandler;
+
+        public ApproveUser(ILogger<ApproveUser> log, IUserService userService, IAuthHandler authHandler)
+        {
+            _logger = log;
+            _userService = userService;
+            _authHandler = authHandler;
+        }
+
+        [FunctionName("ApproveUser")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, )
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            
+        }
+    }
+}
+
