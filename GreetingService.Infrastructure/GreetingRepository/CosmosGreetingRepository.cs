6cfc4680dfa0f94ff12c98dc1ce4c6b80d4638b7
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-17 13:42:47 +0100

    Update CosmosGreetingRepository.cs

--- GreetingService.Infrastructure/GreetingRepository/CosmosGreetingRepository.cs
+++ GreetingService.Infrastructure/GreetingRepository/CosmosGreetingRepository.cs
@@ -1,15 +1,31 @@
 ﻿using GreetingService.Core.Entities;
 using GreetingService.Core.Interfaces;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace GreetingService.Infrastructure.GreetingRepository
 {
     public class CosmosGreetingRepository : IGreetingRepository
     {
+        private const string _cosmosContainerName = "greetingscdb";
+        //private const string _cosmosContainerCsvName = "greetings-csv";
+        private readonly CosmosClient _blobContainerClient;
+        private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true, };
+        private readonly string _connectionString;
+
+        public CosmosGreetingRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["LogStorageAccount"];
+            _cosmosContainerClient = new CosmosClient(_connectionString, _cosmosContainerName);
+            _cosmosContainerClient.CreateIfNotExists();
+        }
+
         public async Task CreateAsync(Greeting greeting)
         {
             throw new NotImplementedException();
