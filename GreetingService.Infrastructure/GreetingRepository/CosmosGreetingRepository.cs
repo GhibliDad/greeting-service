f3fce4bdec9befcc33e486005ab32b50b7b7085d
Shimizu Towa (BT) <towa.shimizu@hm.com>
2022-03-21 11:43:50 +0100

    Update CosmosGreetingRepository.cs

--- GreetingService.Infrastructure/GreetingRepository/CosmosGreetingRepository.cs
+++ GreetingService.Infrastructure/GreetingRepository/CosmosGreetingRepository.cs
@@ -2,6 +2,7 @@
 using GreetingService.Core.Interfaces;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,20 +14,22 @@ namespace GreetingService.Infrastructure.GreetingRepository
 {
     public class CosmosGreetingRepository : IGreetingRepository
     {
+        private readonly ILogger logger;
         private const string _cosmosDbName = "greetingscsd";
         private const string _cosmosContainerName = "greetings";
         private readonly CosmosClient _cosmosClient;
         private readonly Container _container;
 
-        public CosmosGreetingRepository(IConfiguration configuration)
+        public CosmosGreetingRepository(IConfiguration configuration, ILogger logger, CosmosClient cosmosClient)
         {
             var _connectionString = configuration["CosmosDbConnectionString"];
-            _cosmosClient = new CosmosClient(_connectionString, "");
+            _cosmosClient = new CosmosClient(_connectionString);
+            _cosmosClient.GetContainer(_cosmosDbName, _cosmosContainerName);
         }
 
         public async Task CreateAsync(Greeting greeting)
         {
-            await _container.CreateItemAsync(greeting, new PartitionKey(greeting.Id));
+            await _container.CreateItemAsync(greeting);
         }
 
         public async Task DeleteAllAsync()
@@ -36,17 +39,32 @@ namespace GreetingService.Infrastructure.GreetingRepository
 
         public async Task DeleteAsync(Guid id)
         {
-            await _container.DeleteItemAsync<Greeting>(id, new PartitionKey(id));
+            await await _container.DeleteItemAsync<Greeting>(id);
         }
 
         public async Task<Greeting> GetAsync(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var response = await _container.ReadItemAsync<Greeting>(id, new PartitionKey(id);
+                return response.Resource;
+            }
+            catch (CosmosException) //For handling item not found and other exceptions
+            {
+                return null;
+            }
         }
 
         public async Task<IEnumerable<Greeting>> GetAsync()
         {
-            throw new NotImplementedException();
+            var query = _container.GetItemQueryIterator<Greeting>();
+            var results = new List<Greeting>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
+            return results;
         }
 
         public async Task<IEnumerable<Greeting>> GetAsync(string from, string to)
